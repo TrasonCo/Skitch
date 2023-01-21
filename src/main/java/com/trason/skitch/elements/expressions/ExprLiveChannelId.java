a83aeb4eaefaa857602b5d4c1037e4b77591b2f5
Danny <dannypischel@hotmail.com>
2023-01-21 18:04:35 +0100

    :sparkles: new event, game change

---
+++ src/main/java/com/trason/skitch/elements/expressions/ExprLiveChannelId.java
@@ -0,0 +1,75 @@
+package com.trason.skitch.elements.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import com.trason.skitch.elements.events.bukkit.*;
+import org.bukkit.event.Event;
+
+public class ExprLiveChannelId extends SimpleExpression<String> {
+
+    static {
+        Skript.registerExpression(ExprLiveChannelId.class, String.class, ExpressionType.SIMPLE,
+            "[event-]livechannelid");
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        return false;
+    }
+
+    @Override
+    protected String[] get(Event event) {
+        if (event instanceof BridgeEventChat) {
+            String channel = ((BridgeEventChat) event).getEvent().getChannel().getId();
+            return new String[]{channel};
+        }
+        else if (event instanceof BridgeEventCheer) {
+            String channel = ((BridgeEventCheer) event).getEvent().getChannel().getId();
+            return new String[]{channel};
+        }
+        else if (event instanceof BridgeEventFollow) {
+            String channel = ((BridgeEventFollow) event).getEvent().getChannel().getId();
+            return new String[]{channel};
+        }
+        else if (event instanceof BridgeEventGiftSub) {
+            String channel = ((BridgeEventGiftSub) event).getEvent().getChannel().getId();
+            return new String[]{channel};
+        }
+        else if (event instanceof BridgeEventGoLive) {
+            String channel = ((BridgeEventGoLive) event).getEvent().getChannel().getId();
+            return new String[]{channel};
+        }
+        else if (event instanceof BridgeEventOffLive) {
+            String channel = ((BridgeEventOffLive) event).getEvent().getChannel().getId();
+            return new String[]{channel};
+        }
+        else if (event instanceof BridgeEventSub) {
+            String channel = ((BridgeEventSub) event).getEvent().getChannel().getId();
+            return new String[]{channel};
+        }
+        else
+            return new String[0];
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public Class<? extends String> getReturnType() {
+        return String.class;
+    }
+
+    @Override
+    public String toString(Event event, boolean debug) {
+        return "event-livechannelid";
+    }
+
+
+
+}
