63623ec120e6c9ddcd4b68c4725ea324fad58b7b
Danny <dannypischel@hotmail.com>
2023-04-16 01:48:12 +0200

    :sparkles: ClipUrl

---
+++ src/main/java/com/trason/skitch/elements/expressions/ExprClipURL.java
@@ -0,0 +1,48 @@
+package com.trason.skitch.elements.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import com.trason.skitch.elements.events.bukkit.BridgeEventClip;
+import org.bukkit.event.Event;
+
+public class ExprClipURL extends SimpleExpression<String> {
+
+    static {
+        Skript.registerExpression(ExprClipTitle.class, String.class, ExpressionType.SIMPLE,
+            "[event-]clipurl");
+    }
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        return true;
+    }
+
+    @Override
+    protected String[] get(Event event) {
+        if (event instanceof BridgeEventClip) {
+            String url = ((BridgeEventClip)event).getEvent().getClip().getUrl();
+            return new String[]{url};
+        }
+        else
+            return new String[0];
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public Class<? extends String> getReturnType() {
+        return String.class;
+    }
+
+    @Override
+    public String toString(Event event, boolean debug) {
+        return "clipurl";
+    }
+}
