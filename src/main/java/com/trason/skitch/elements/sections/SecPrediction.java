9032ecbd1fcc44dfb2c26357fd66c3e0d5a88f12
Danny <dannypischel@hotmail.com>
2023-04-18 19:46:51 +0200

    feat(master): Added Docs
    Added Name, Description, Example

--- src/main/java/com/trason/skitch/elements/sections/SecPrediction.java
+++ src/main/java/com/trason/skitch/elements/sections/SecPrediction.java
@@ -2,16 +2,28 @@ package com.trason.skitch.elements.sections;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.config.SectionNode;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.Section;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.TriggerItem;
 import ch.njol.util.Kleenean;
 import com.github.twitch4j.helix.domain.Prediction;
-import org.apache.commons.lang3.exception.ContextedRuntimeException;
 import org.bukkit.event.Event;
 
 import java.util.List;
+
+@Name("Prediction")
+@Description("Create a new prediction")
+@Examples("make new prediction:\n" +
+    "\tset prediction target of last prediction to event-livechannelid\n" +
+    "\tset prediction title of last prediction to \"test\"\n" +
+    "\tadd \"test1\" to prediction outcome of last prediction\n" +
+    "\tadd \"test2\" to prediction outcome of last prediction\n" +
+    "\tset prediction time of last prediction to 10\n" +
+    "send last prediction to livechat event-livechannel")
 
 public class SecPrediction extends Section {
 
