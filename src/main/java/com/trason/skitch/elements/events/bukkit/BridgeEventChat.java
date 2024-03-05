bd4da8e6cae37615e120e91157476c8c8592efc4
Danny <dannypischel@hotmail.com>
2024-03-05 14:42:03 +0100

    :zap: added async

--- src/main/java/com/trason/skitch/elements/events/bukkit/BridgeEventChat.java
+++ src/main/java/com/trason/skitch/elements/events/bukkit/BridgeEventChat.java
@@ -1,16 +1,20 @@
 package com.trason.skitch.elements.events.bukkit;
 
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+import java.util.function.Consumer;
 
 public class BridgeEventChat extends Event {
 
     private static final HandlerList handlers = new HandlerList();
 
     private final ChannelMessageEvent event;
-
 
     public BridgeEventChat(ChannelMessageEvent event) {
         this.event = event;
@@ -30,6 +34,13 @@ public class BridgeEventChat extends Event {
         return handlers;
     }
 
+    public void runAsyncTask(Consumer<BridgeEventChat> callback) {
+        // Use BukkitRunnable to run the callback asynchronously
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                callback.accept(BridgeEventChat.this);
+            }
+        }.runTaskAsynchronously(Objects.requireNonNull(Bukkit.getPluginManager().getPlugin("Skitch")));
+    }
 }
-
-
